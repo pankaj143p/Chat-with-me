305ef8cb664ecffbd7ad08a4d9cf3129ec258862
pankaj143p <pankaj114477pankaj@gmail.com>
2025-08-30 16:15:14 +0530

    bug fixes

--- server/controller/checkPassword.js
+++ server/controller/checkPassword.js
@@ -21,7 +21,7 @@ async function checkPassword(request,response){
             id : user._id,
             email : user.email 
         }
-        const token = await jwt.sign(tokenData,process.env.JWT_SECREAT_KEY,{ expiresIn : '1d'})
+        const token = await jwt.sign(tokenData,process.env.JWT_SECRET_KEY,{ expiresIn : '1d'})
 
         const cookieOptions = {
             http : true,
--- server/helpers/getUserDetailsFromToken.js
+++ server/helpers/getUserDetailsFromToken.js
@@ -10,7 +10,7 @@ const getUserDetailsFromToken = async(token)=>{
         }
     }
 
-    const decode = await jwt.verify(token,process.env.JWT_SECREAT_KEY)
+    const decode = await jwt.verify(token,process.env.JWT_SECRET_KEY)
 
     const user = await UserModel.findById(decode.id).select('-password')
 
